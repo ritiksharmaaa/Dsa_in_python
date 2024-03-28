12f29be6ee700639120c94ee5d9245d962b5759f
Ritik sharma <ritik1832004@gmail.com>
2024-03-28 23:11:44 +0530

    initial commit

---
+++ recursion/factorial_function.py
@@ -0,0 +1,15 @@
+# what is factorial : mean if  factorial mean multiple of 5 * 4 * 3 * 2*1 
+#  in formuly n! = n*(n-1)*(n-2)*(n-3)*(n-4) like that 
+#  factorial help to find permutation and combination  if a password  a b c so how mnay combination it take to make so her 
+# 3!= 3*2*1 = 6 there is total 6 combination made . 
+
+
+def factorial(n):
+    if n == 0 :
+        return 1 
+    else : 
+         return n * factorial(n-1)
+
+
+data = factorial(12)
+print(data)
